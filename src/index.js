dbafaf158566a8763c4d3e6b1ddb429ee5fad481
Cristian Sánchez <cristian.asm19@gmail.com>
2021-11-28 13:30:08 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bp = require('body-parser');
+const app = express();
+const fs = require("fs");
+const port = process.env.PORT || 3000;
+const appointments = require('../data/appointments.json');
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.send('ChatBot Automatización 2021 - 2 (Cristian Sánchez)');
+});
+
+app.get('/appointments', function (req, res) {
+  res.json(appointments);
+});
+
+// POST method route
+app.post("/save", function (req, res) {
+  console.log(req.body);
+  saveFile(req.body, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(400).send("Error saving file");
+      return;
+    }
+    res.send("saved");
+  });
+});
+
+app.listen(port, function() {
+  console.log('server up and running at port: %s', port);
+});
+
+function saveFile(data, callback) {
+  fs.writeFile("./data/appointments.json", JSON.stringify(data), callback);
+}
